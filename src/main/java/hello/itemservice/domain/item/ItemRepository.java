1da5a887c795f07e07db335dcd3be037d90db028
jiminKim <programmer0415@gmail.com>
2022-04-19 09:40:12 +0900

    Feat: 아이템 도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,41 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>(); //동시 여러개 사용. ConcurrentHashMap 쓰레드 동기화..
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    //여긴 원래 ItemDto를 만들어서 .. 전달해 줘야 한다.
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
+
